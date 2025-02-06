2930b2b17a7abe3b2ce0097b7200278dc3f55911
水草 <a760688599@outlook.com>
2025-02-06 22:05:20 +0800

    QQ

---
+++ src/main/java/com/leisure/tree/DFS.java
@@ -0,0 +1,15 @@
+package com.leisure.tree;
+
+/**
+ * ClassName: DFS Depth-First Search 深度优先算法
+ * Package: com.leisure.tree 使用了树结构的算法
+ * Description:全排列算法
+ *
+ * @Author: MyLeisureLife
+ * @Date: 2025/2/6:22:00:51 星期四
+ */
+public class DFS {
+    public static void main(String[] args) {
+        System.out.println("DFS");
+    }
+}
