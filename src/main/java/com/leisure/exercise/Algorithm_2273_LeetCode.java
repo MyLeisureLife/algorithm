fe51ac29f0e91670a47d91d4440d7a95d9d938dd
水草 <a760688599@outlook.com>
2025-10-18 19:49:57 +0800

    1

--- src/main/java/com/leisure/exercise/Algorithm_2273_LeetCode.java
+++ src/main/java/com/leisure/exercise/Algorithm_2273_LeetCode.java
@@ -68,7 +68,22 @@ public class Algorithm_2273_LeetCode {
         return true;
     }
 
+    
 
+
+    public boolean isAnagram2(String s, String t) {
+        int[] count = new int[26];
+        for( char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+        for( char c : t.toCharArray()) {
+            count[c - 'a']--;
+        }
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) return false;
+        }
+        return true;
+    }
 
 
 
